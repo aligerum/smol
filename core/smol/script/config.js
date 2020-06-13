e4f420461cfbbddb232cafb3406a1ddfef8cabd8
Allister Aligerum <allister@aligerum.io>
2020-06-13 16:10:54 -0400

    properly load config

--- core/smol/script/config.js
+++ core/smol/script/config.js
@@ -1,13 +1,21 @@
 let fs = require('fs')
+let configs = []
 
 // get config by name
 module.exports = name => {
   if (!name) name = 'smol'
+  if (configs[name]) return configs[name]
   let paths = [`${process.cwd()}/config/${name}.json`, `${process.cwd()}/template/${name}.config.json`]
   if (fs.existsSync(`${process.cwd()}/core/${name}`)) {
     let coreJson = require(`${process.cwd()}/core/${name}/core.json`)
-    let configPath = `${__dirname}/../../${coreJson.type}/template/config.json`
+    let command = require('./command')
+    let configPath = `${command.corePrototypes.find(prototype => prototype.name == coreJson.type).path}/template/config.json`
     if (fs.existsSync(configPath)) paths.push(configPath)
   }
-  for (let path of paths) if (fs.existsSync(path)) return require(path)
+  for (let path of paths) {
+    if (fs.existsSync(path)) {
+      configs[name] = require(path)
+      return configs[name]
+    }
+  }
 }
